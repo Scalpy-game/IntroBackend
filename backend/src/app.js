639b5cc96a332dbab4acf32c69bafcc5b0dbfcfe
Scalpy-game <bilayoda40@gmail.com>
2025-12-09 22:49:19 +0000

    Feat: add CRUD

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,11 @@ const app = express(); // create an express app
 app.use(express.json());
 //routs import
 import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 //ewample route: http://localhost:4000/api/v1/users/register
 
